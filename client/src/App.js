71f3ff60ebd838d509ab5b69150584691f157951
Najia Jahan <naziausha@gmail.com>
2024-03-16 21:35:54 -0400

    profile page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css'; // Assuming you have some CSS file for styling
 import Navbar from './Navbar/Navbar';
 import Join from './Pages/JoinUs/Join'; 
 import Home from './Pages/HomePage/Home';
+import PersonalProfile from './Pages/Profile/PersonalProfile';
 
 function App() {
   const [backendData, setBackendData] = useState([]);
@@ -39,7 +40,8 @@ function App() {
               </div>
             } />
             <Route path="/join" element={<Join />} /> 
-            <Route path="/home" element={<Home />} /> 
+            <Route path="/home" element={<Home />} />
+            <Route path="/personalprofile" element={<PersonalProfile />} /> 
           </Routes>
         </div>
       </div>
---
+++ client/src/Pages/Profile/PersonalProfile.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './PersonalProfile.css'; // Importing CSS file
+
+export default function PersonalProfile() {
+  return (
+    <section className="profile-section">
+      <div className="profile-container">
+        <div className="profile-info">
+          <div className="profile-picture">
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp" alt="Avatar" />
+          </div>
+          <div className="profile-details">
+            <h2 className="profile-name">Marie Horwitz</h2>
+            <p className="profile-occupation">Web Designer</p>
+          </div>
+        </div>
+        <div className="profile-bio">
+          <div className="profile-info">
+            <p><strong>Location:</strong> New York, USA</p>
+          </div>
+          <div className="profile-info">
+
+            <p><strong>Email:</strong> info@example.com</p>
+            <p><strong>Phone:</strong> 123 456 789</p>
+          </div>
+          <h3>Skills</h3>
+          <div className="profile-bubbles">
+            <span className="profile-bubble">Web Design</span>
+            <span className="profile-bubble">Graphic Design</span>
+            <span className="profile-bubble">Frontend Development</span>
+          </div>
+          <h3>Interests</h3>
+          <div className="profile-bubbles">
+            <span className="profile-bubble">Coding</span>
+            <span className="profile-bubble">Reading</span>
+            <span className="profile-bubble">Hiking</span>
+          </div>
+          <h3>Gallery</h3>
+          <div className="profile-gallery">
+            <img src="https://via.placeholder.com/150" alt="Gallery Image 1" />
+            <img src="https://via.placeholder.com/150" alt="Gallery Image 2" />
+            <img src="https://via.placeholder.com/150" alt="Gallery Image 3" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
