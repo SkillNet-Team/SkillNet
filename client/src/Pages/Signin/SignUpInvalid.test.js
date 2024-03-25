7b0283c810f51ed0c2bebb34599f2892921de95e
Myesha <maisha.mahjabin27@gmail.com>
2024-03-25 19:09:51 -0400

    Unit Test, Integration Test & E2E Test

---
+++ client/src/Pages/Signin/SignUpInvalid.test.js
@@ -0,0 +1,58 @@
+// signup.test.js
+
+import React from 'react';
+import { render, fireEvent, waitFor, act } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import SignUp from './SignUp';
+
+describe('Signup integration tests', () => {
+  it('should allow user to sign up with valid credentials', async () => {
+    // Mock the fetch function to return a successful response
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({
+      ok: false,
+      json: () => Promise.resolve({ message: 'User created failed!' }),
+    });
+    // jest.spyOn(global, 'fetch')
+
+    const { getByLabelText, getByText } = render(
+      <Router>
+        <SignUp />
+      </Router>
+    );
+
+    // Fill out the form with valid credentials
+    
+    fireEvent.change(getByLabelText('First Name'), { target: { value: 'jow' } });
+    fireEvent.change(getByLabelText('Last Name'), { target: { value: 'drop' } });
+    fireEvent.change(getByLabelText('Email'), { target: { value: 'rufus@example.com' } });
+    fireEvent.change(getByLabelText('Password'), { target: { value: 'password' } });
+    fireEvent.change(getByLabelText('Confirm Password'), { target: { value: '' } });
+
+    // Submit the form
+    await act(async () => {
+        fireEvent.click(getByText('sign up'))
+    });
+
+    // Wait for the submission to be processed
+    await waitFor(() => {
+      // Ensure fetch is called with the correct endpoint and request body
+      expect(global.fetch).toHaveBeenCalledWith(
+        'http://localhost:5000/api/users/signup',
+        {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            firstName: 'jow',
+            lastName: 'drop',
+            email: 'rufus@example.com',
+            password: 'password',
+            confirmPassword: ''
+          }),
+        });
+    });
+    console.log(document.body.innerHTML)
+
+    // Verify that success message is displayed
+    expect(getByText('Already have an account?')).toBeInTheDocument();
+  });
+});
