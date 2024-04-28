13b29bf536a62699fb1af25bf1eb3154e898fd23
Myesha <maisha.mahjabin27@gmail.com>
2024-04-27 22:15:33 -0400

    updates

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -2,12 +2,6 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-<<<<<<< HEAD
-    email: {type: String, required: true, unique: true},
-    firstName: {type: String, required: true, unique: false},
-    lastName: {type: String, required: true, unique: false},
-    password: {type: String, required: true, unique: false}
-=======
     email: { type: String, required: true, unique: true },
     firstName: { type: String, required: true, unique: false },
     lastName: { type: String, required: true, unique: false },
@@ -21,7 +15,6 @@ const userSchema = new mongoose.Schema({
     profilePicture: { type: String, required: false, unique: false, default: "" },
     requests: { type: [String], required: false, unique: false },
     matches: { type: [String], required: false, unique: false }
->>>>>>> e707bdb4d9c93350cf2014892f901a062b1100c2
 });
 
 userSchema.pre('save', async function (next) {
