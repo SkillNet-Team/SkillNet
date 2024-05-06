3c1a406913480c9ecff14e6e044e058981106fff
Najia Jahan <naziausha@gmail.com>
2024-05-06 01:33:57 -0400

    modified .sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-zip -r "flaskbb_deploy-$1.zip" ./flaskbb ./.ebextensions wsgi.py setup.py setup.cfg requirements.txt flaskbb.cfg celery_worker.py
+zip -r "skillnet-deploy-${{github.sha}}.zip"
 
-aws s3 cp "flaskbb_deploy-$1.zip" s3://flaskbbapp
+aws s3 cp "flaskbb_deploy-$1.zip" s3://najia096-skillnet
 
-aws elasticbeanstalk create-application-version --application-name flaskbb --source-bundle S3Bucket="flaskbbapp",S3Key="flaskbb_deploy-$1.zip" --version-label "ver-$1" --description "file permissions" --region "us-east-1"
+aws elasticbeanstalk create-application-version --application-name skillnet --source-bundle S3Bucket="najia096-skillnet",S3Key="skillnet_deploy-$1.zip" --version-label "ver-$1" --description "file permissions" --region "us-east-1"
 
-aws elasticbeanstalk update-environment --environment-name flaskbb-environment --version-label "ver-$1" --region "us-east-1"
+aws elasticbeanstalk update-environment --environment-name skillnet-environment --version-label "ver-$1" --region "us-east-1"
