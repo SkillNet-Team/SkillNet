acd512e2dfd8b3b5d17d83db8ed1860dcdb3b643
Najia Jahan <naziausha@gmail.com>
2024-05-06 12:10:39 -0400

    .sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 zip -r "skillnet-deploy-${{github.sha}}.zip"
 
-aws s3 cp "flaskbb_deploy-$1.zip" s3://najia096-skillnet
+aws s3 cp "skillnet_deploy-$1.zip" s3://najia096-skillnet
 
 aws elasticbeanstalk create-application-version --application-name skillnet --source-bundle S3Bucket="najia096-skillnet",S3Key="skillnet_deploy-$1.zip" --version-label "ver-$1" --description "file permissions" --region "us-east-1"
 
