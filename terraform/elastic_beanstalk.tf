f308f5832887ea4a1d6ffad5be5c13c412fabae9
tahbee03 <talikeb20@gmail.com>
2024-05-05 22:07:07 -0400

    created terraform files

---
+++ terraform/elastic_beanstalk.tf
@@ -0,0 +1,15 @@
+resource "aws_elastic_beanstalk_application" "application" {
+    name = "skillnet"  
+}
+
+resource "aws_elastic_beanstalk_environment" "environment" {
+  name                = "skillnet-environment"
+  cname_prefix        = "tahbee03skillnet"
+  application         = aws_elastic_beanstalk_application.application.name
+  solution_stack_name = "64bit Amazon Linux 2023 v6.1.4 running Node.js 20"
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+}
