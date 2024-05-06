8d50056efbca63b3eac53dacdaa07f0a0cd958b4
Najia Jahan <naziausha@gmail.com>
2024-05-06 01:22:24 -0400

    flaskbb

--- Terraform/elastic_beanstalk.tf
+++ Terraform/elastic_beanstalk.tf
@@ -1,10 +1,10 @@
 resource "aws_elastic_beanstalk_application" "application" {
-    name = "flaskbb"  
+    name = "skillnet"  
 }
 
 resource "aws_elastic_beanstalk_environment" "environment" {
-  name                = "flaskbb-environment"
-  cname_prefix        = "najia096flaskbb"
+  name                = "skillnet-environment"
+  cname_prefix        = "najia096skillnet"
   application         = aws_elastic_beanstalk_application.application.name
   solution_stack_name = "64bit Amazon Linux 2023 v6.1.4 running Node.js 20"
   setting {
